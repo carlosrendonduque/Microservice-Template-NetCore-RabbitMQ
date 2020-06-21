d604c01f8616999c17f0a05a6843986ccddc45b9
Carlos Rendon <carlos@zen-it.solutions>
2020-06-20 22:53:55 -0500

    Initial commit Infrastructurr Dependency Project

---
+++ RabbitMQ.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,21 @@
+﻿using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Infra.Bus;
+using Microsoft.Extensions.DependencyInjection;
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Text;
+
+namespace RabbitMQ.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            //Domain bus
+            services.AddTransient<IEventBus, RabbitMQBus>();
+
+        }
+    }
+}
