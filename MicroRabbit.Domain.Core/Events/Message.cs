2192955212c62176fda56e0b7d08f8c7d8f00daf
Carlos Rendon <carlos@zen-it.solutions>
2020-06-20 19:25:45 -0500

    Implentation od Interfaces and classes for Domain Core

---
+++ MicroRabbit.Domain.Core/Events/Message.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Net;
+using System.Text;
+
+namespace MicroRabbit.Domain.Core.Events
+{
+    public abstract class Message : IRequest<bool>
+    {
+        public String MessageType { get; protected set; }
+        protected Message()
+        {
+            MessageType = GetType().Name;
+
+        }
+    }
+}
